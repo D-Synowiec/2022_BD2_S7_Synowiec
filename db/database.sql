00b40f707da23f6d74d9a6ce0d3df64ecc4fee7c
Daniel S <d.syn2600@gmail.com>
2022-12-05 22:15:34 +0100

    Fixed version of DB

--- db/database.sql
+++ db/database.sql
@@ -5,14 +5,15 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS public."Users"
 (
-    id integer NOT NULL,
+    idu integer NOT NULL,
     primary_name character varying(20) NOT NULL,
     second_name character varying(20) NOT NULL,
     email character varying(65)[] NOT NULL,
     login character varying(20) NOT NULL,
     password_hash character varying(320) NOT NULL,
     role integer NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (idu),
+    UNIQUE (idu)
 );
 
 COMMENT ON TABLE public."Users"
@@ -20,9 +21,10 @@ COMMENT ON TABLE public."Users"
 
 CREATE TABLE IF NOT EXISTS public."Roles"
 (
-    id integer NOT NULL,
+    idr integer NOT NULL,
     role_name character varying(25) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (idr),
+    UNIQUE (idr)
 );
 
 COMMENT ON TABLE public."Roles"
@@ -30,52 +32,60 @@ COMMENT ON TABLE public."Roles"
 
 CREATE TABLE IF NOT EXISTS public."Gallery"
 (
-    id integer NOT NULL,
+    idg integer NOT NULL,
     gallery_owner integer NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (idg),
+    CONSTRAINT id UNIQUE (idg)
 );
 
 CREATE TABLE IF NOT EXISTS public."Gallery_Access"
 (
     "user" integer NOT NULL,
-    gallery integer NOT NULL
+    gallery integer NOT NULL,
+    idga integer NOT NULL,
+    PRIMARY KEY (idga),
+    UNIQUE (idga)
 );
 
 CREATE TABLE IF NOT EXISTS public."Backup"
 (
-    id integer NOT NULL,
+    idb integer NOT NULL,
     gallery integer NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (idb),
+    UNIQUE (idb)
 );
 
 CREATE TABLE IF NOT EXISTS public."Media"
 (
-    id integer NOT NULL,
+    idm integer NOT NULL,
     type integer NOT NULL,
     creation_date date NOT NULL,
     is_av boolean NOT NULL,
     backup_id integer NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (idm),
+    UNIQUE (idm)
 );
 
 CREATE TABLE IF NOT EXISTS public."Media_Dictonary"
 (
-    id integer NOT NULL,
+    idmd integer NOT NULL,
     name character varying(20) NOT NULL,
     size integer NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (idmd),
+    UNIQUE (idmd)
 );
 
 CREATE TABLE IF NOT EXISTS public."Photo"
 (
-    id integer NOT NULL,
+    idp integer NOT NULL,
     owner character varying(32) NOT NULL,
     name character varying(32) NOT NULL,
     size integer NOT NULL,
     resolution character varying(10) NOT NULL,
     extension character varying(5) NOT NULL,
     galleries integer[] NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (idp),
+    UNIQUE (idp)
 );
 
 CREATE TABLE IF NOT EXISTS public."Tags"
@@ -86,10 +96,11 @@ CREATE TABLE IF NOT EXISTS public."Tags"
 
 CREATE TABLE IF NOT EXISTS public."Categories"
 (
-    id integer NOT NULL,
+    idc integer NOT NULL,
     name character varying NOT NULL,
     parent_category integer,
-    PRIMARY KEY (id)
+    PRIMARY KEY (idc),
+    UNIQUE (idc)
 );
 
 CREATE TABLE IF NOT EXISTS public."Categorized_Photos"
@@ -100,31 +111,31 @@ CREATE TABLE IF NOT EXISTS public."Categorized_Photos"
 
 ALTER TABLE IF EXISTS public."Users"
     ADD FOREIGN KEY (role)
-    REFERENCES public."Roles" (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public."Users"
-    ADD FOREIGN KEY (id)
-    REFERENCES public."Gallery_Access" ("user") MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public."Users"
-    ADD FOREIGN KEY (id)
-    REFERENCES public."Gallery" (gallery_owner) MATCH SIMPLE
+    REFERENCES public."Roles" (idr) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS public."Gallery"
-    ADD FOREIGN KEY (id)
-    REFERENCES public."Gallery_Access" (gallery) MATCH SIMPLE
+    ADD FOREIGN KEY (gallery_owner)
+    REFERENCES public."Users" (idu) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."Gallery_Access"
+    ADD FOREIGN KEY ("user")
+    REFERENCES public."Users" (idu) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."Gallery_Access"
+    ADD FOREIGN KEY (gallery)
+    REFERENCES public."Gallery" (idg) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
@@ -132,7 +143,7 @@ ALTER TABLE IF EXISTS public."Gallery"
 
 ALTER TABLE IF EXISTS public."Backup"
     ADD FOREIGN KEY (gallery)
-    REFERENCES public."Gallery" (id) MATCH SIMPLE
+    REFERENCES public."Gallery" (idg) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
@@ -140,7 +151,7 @@ ALTER TABLE IF EXISTS public."Backup"
 
 ALTER TABLE IF EXISTS public."Media"
     ADD FOREIGN KEY (type)
-    REFERENCES public."Media_Dictonary" (id) MATCH SIMPLE
+    REFERENCES public."Media_Dictonary" (idmd) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
@@ -148,15 +159,7 @@ ALTER TABLE IF EXISTS public."Media"
 
 ALTER TABLE IF EXISTS public."Media"
     ADD FOREIGN KEY (backup_id)
-    REFERENCES public."Backup" (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public."Photo"
-    ADD FOREIGN KEY (galleries)
-    REFERENCES public."Gallery" (id) MATCH SIMPLE
+    REFERENCES public."Backup" (idb) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
@@ -164,7 +167,7 @@ ALTER TABLE IF EXISTS public."Photo"
 
 ALTER TABLE IF EXISTS public."Tags"
     ADD FOREIGN KEY (photo_id)
-    REFERENCES public."Photo" (id) MATCH SIMPLE
+    REFERENCES public."Photo" (idp) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
@@ -172,7 +175,7 @@ ALTER TABLE IF EXISTS public."Tags"
 
 ALTER TABLE IF EXISTS public."Categories"
     ADD FOREIGN KEY (parent_category)
-    REFERENCES public."Categories" (id) MATCH SIMPLE
+    REFERENCES public."Categories" (idc) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
@@ -180,7 +183,7 @@ ALTER TABLE IF EXISTS public."Categories"
 
 ALTER TABLE IF EXISTS public."Categorized_Photos"
     ADD FOREIGN KEY (photo)
-    REFERENCES public."Photo" (id) MATCH SIMPLE
+    REFERENCES public."Photo" (idp) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
@@ -188,7 +191,7 @@ ALTER TABLE IF EXISTS public."Categorized_Photos"
 
 ALTER TABLE IF EXISTS public."Categorized_Photos"
     ADD FOREIGN KEY (category)
-    REFERENCES public."Categories" (name) MATCH SIMPLE
+    REFERENCES public."Categories" (idc) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
