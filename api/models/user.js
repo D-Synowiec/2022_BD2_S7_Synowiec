bc9252461fbc3d58ae3e39d08d88962e9007e423
Anrsh <d.syn2600@gmail.com>
2023-01-11 20:23:21 +0100

    Basic user model

---
+++ api/models/user.js
@@ -0,0 +1,56 @@
+const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config()
+
+
+const sequelize = new Sequelize(process.env.PGLINK);
+
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    primary_name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+    },
+    second_name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING(65),
+        allowNull: false,
+    },
+    login: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+    },
+    password_hash: {
+        type: DataTypes.STRING(320),
+        allowNull: false,
+    },
+    role: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    tokens: {
+        type: DataTypes.ARRAY(DataTypes.STRING)
+    }
+
+}, {
+    timestamps: false
+})
+
+
+async function test() {
+    try {
+      const users = await User.findAll()
+        console.log("All users:", JSON.stringify(users, null, 2));
+    } catch (e) {
+        console.error(e);
+    }
+}
+test();
