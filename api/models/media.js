47a0b68d3259ba38183c115063853ccad531d565
Krystian <stochkrystian@tlen.pl>
2023-01-12 12:34:11 +0100

    :)

--- api/models/media.js
+++ api/models/media.js
@@ -1,33 +1,52 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config()
+
+
 const sequelize = new Sequelize(process.env.PGLINK);
-const Category = sequelize.define('Category', {
+
+
+const x = sequelize.define('Media', {
     id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         autoIncrement: true,
         primaryKey: true
     },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    parent_category: {
+    type: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        autoIncrement: true,
         references:{
-            model: Category,
+            model: Media_Dictonary,
             key: 'id'
         }
-    }
+    },
+    creation_date: {
+        type: DataTypes.DATA,
+        allowNull: false
+    },
+    is_av: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    backup_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        references:{
+            model: Backup,
+            key: 'id'
+        }
+    },
 
 }, {
     timestamps: false
 });
 
+
 async function test() {
     try {
-      const users = await Category.findAll()
+      const users = await User.findAll()
         console.log("All users:", JSON.stringify(users, null, 2));
     } catch (e) {
         console.error(e);
